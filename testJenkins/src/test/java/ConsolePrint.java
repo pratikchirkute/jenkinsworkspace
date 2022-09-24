3874c6afd537d96567c228ba8cc57160bac8371b
pratik chirkute <pratik.chirkute@gmail.com>
2022-09-25 01:17:45 +0200

    Jenkins Testing

---
+++ testJenkins/src/test/java/ConsolePrint.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class ConsolePrint {
+    @Test
+    public void logPrinter(){
+        System.out.println("First Jenkins Job Execution Testing");
+    }
+}
