892ca40e4989343df649be5c348ca9ded0e6e4d2
pratik chirkute <pratik.chirkute@gmail.com>
2022-09-26 22:28:30 +0200

    optimized web base classes

--- src/test/java/webTest.java
+++ src/test/java/webTest.java
@@ -4,13 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class webTest extends Web {
 
     @Test
     public void sauceWebCheck() throws IOException {
         WebDriver driver = getDriver();
-
         driver.get(getProperty("base.url.saucedemo"));
         Assert.assertTrue("Swag Labs".matches(driver.getTitle()));
         driver.quit();
